dc91df8f9a2365247edbf3b8423d2359d054f5cd
Elan Ruusamäe <glen@delfi.ee>
2013-11-27 22:40:12 +0200

    start with 3.0.6

--- check_raid.pl
+++ check_raid.pl
@@ -3486,7 +3486,7 @@ use Getopt::Long;
 
 my ($opt_V, $opt_d, $opt_h, $opt_W, $opt_S, $opt_p, $opt_l);
 my (%ERRORS) = (OK => 0, WARNING => 1, CRITICAL => 2, UNKNOWN => 3);
-my ($VERSION) = "3.0.5";
+my ($VERSION) = "3.0.6";
 my ($message, $status, $perfdata, $longoutput);
 my ($opt_O) = $ERRORS{UNKNOWN};
 
