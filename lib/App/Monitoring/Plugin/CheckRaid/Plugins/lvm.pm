5089b2011073b1dd6796f6c4d83757c65a908b48
Elan Ruusamäe <glen@delfi.ee>
2016-03-07 23:46:13 +0200

    lvm: some doc, warn if device is not in sync

--- lib/App/Monitoring/Plugin/CheckRaid/Plugins/lvm.pm
+++ lib/App/Monitoring/Plugin/CheckRaid/Plugins/lvm.pm
@@ -1,6 +1,7 @@
 package App::Monitoring::Plugin::CheckRaid::Plugins::lvm;
 
 # Package to check Linux LVM RAID
+# https://www.kernel.org/doc/Documentation/device-mapper/dm-raid.txt
 
 use base 'App::Monitoring::Plugin::CheckRaid::Plugin';
 use strict;
@@ -31,14 +32,9 @@ sub parse {
 	my $this = shift;
 
 	my @columns = qw[
-		dmname s l
-		raid
-		raidraid_type
-		devices
-		health_chars
-		sync_ratio
-		sync_action
-		mismatch_cnt
+		dmname s l raid
+		raid_type devices health_chars
+		sync_ratio sync_action mismatch_cnt
 	];
 
 	my @devices;
@@ -61,11 +57,21 @@ sub check {
 	my @status;
 	foreach my $dm (@$c)
 	{
-		if ($dm->{health_chars} =~ /D/)
-		{
-			$this->critical;
-		}
+		# <health_chars>  One char for each device, indicating:
+		# 'A' = alive and in-sync,
+		# 'a' = alive but not in-sync,
+		# 'D' = dead/failed.
+		$this->critical if ($dm->{health_chars} =~ /D/);
+		$this->warning if ($dm->{health_chars} =~ /a/);
 
+		# <sync_action>   One of the following possible states:
+		# idle    - No synchronization action is being performed.
+		# frozen  - The current action has been halted.
+		# resync  - Array is undergoing its initial synchronization or...
+		# recover - A device in the array is being rebuilt or...
+		# check   - A user-initiated full check of the array is...
+		# repair  - The same as "check", but discrepancies are...
+		# reshape - The array is undergoing a reshape.
 		if ($dm->{sync_action} =~ /^(check|repair|init)$/)
 		{
 			$this->warning;
